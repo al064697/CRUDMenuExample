e5b3af37a0f39a833c58adccdadeac86afb49169
freligio008 <freligio008@icloud.com>
2024-03-04 23:50:00 -0600

    global changes in CRUDMenuExample

--- src/CRUDMenuExample.java
+++ src/CRUDMenuExample.java
@@ -27,6 +27,40 @@ public class CRUDMenuExample {
                     // Crear cuenta
                     System.out.print("Ingrese el nombre de la cuenta: ");
                     String name = new Scanner(System.in).nextLine();
+                    accounts.add(new Accounts(name));
+                    break;
+                case 2:
+                    // Leer cuentas
+                    for (Accounts account : accounts) {
+                        System.out.println(account.getName());
+                    }
+                    break;
+                case 3:
+                    // Actualizar cuenta
+                    System.out.print("Ingrese el nombre de la cuenta a actualizar: ");
+                    String oldName = new Scanner(System.in).nextLine();
+                    System.out.print("Ingrese el nuevo nombre de la cuenta: ");
+                    String newName = new Scanner(System.in).nextLine();
+                    for (Accounts account : accounts) {
+                        if (account.getName().equals(oldName)) {
+                            account.setName(newName);
+                            break;
+                        }
+                    }
+                    break;
+                case 4:
+                    // Eliminar cuenta
+                    System.out.print("Ingrese el nombre de la cuenta a eliminar: ");
+                    String nameToDelete = new Scanner(System.in).nextLine();
+                    accounts.removeIf(account -> account.getName().equals(nameToDelete));
+                    break;
+                case 5:
+                    // Salir
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+                    break;
             }
         }
     }
